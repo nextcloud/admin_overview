4db766195e9b270042e70eca99f2d1404a413d50
Ferdinand Thiessen <opensource@fthiessen.de>
2023-10-12 16:01:54 +0200

    chore: Init repository with app skeleton
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ appinfo/routes.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+/*
+	SPDX-FileCopyrightText: 2023 Ferdinand Thiessen <opensource@fthiessen.de>
+	SPDX-License-Identifier: CC0-1.0
+*/
+return [
+	'routes' => [
+	],
+];
---
+++ lib/AppInfo/Application.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+// SPDX-FileCopyrightText: Ferdinand Thiessen <opensource@fthiessen.de>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OCA\AdminOverview\AppInfo;
+
+use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+
+class Application extends App implements IBootstrap {
+	public const APP_ID = 'admin_overview';
+
+	public function __construct(array $params = []) {
+		parent::__construct(self::APP_ID, $params);
+	}
+
+	public function register(IRegistrationContext $context): void {
+		// Register the composer autoloader for packages shipped by this app, if applicable
+		include_once __DIR__ . '/../../vendor/autoload.php';
+	}
+
+	/**
+	 * Boot Logic
+	 * @param IBootContext $context
+	 */
+	public function boot(IBootContext $context): void {
+		// No boot logic here yet...
+	}
+}
