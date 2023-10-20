a69e14cd34dcfe35e11ce1cf22f89d36eaddd442
Côme Chilliet <come.chilliet@nextcloud.com>
2023-10-20 11:16:27 +0200

    fix(ci): Update workflows from nextcloud/.github
    * Update phpunit workflows from nextcloud/.github
    * Add workflows from nextcloud/.github
    * Add missing licenses for reuse compliance
    
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+/*
+ * SPDX-FileCopyrightText: 2023 Côme Chilliet <come.chilliet@nextcloud.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 require_once './vendor/autoload.php';
 
 use Nextcloud\CodingStandard\Config;
