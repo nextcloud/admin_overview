05d647abf6692fff74706938f7e1d7bb30afdcde
Ferdinand Thiessen <opensource@fthiessen.de>
2023-10-24 19:11:47 +0200

    feat(ui): Add initial settings section for admin overview
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ src/vue.d.ts
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: Ferdinand Thiessen <opensource@fthiessen.de>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare module '*.svg?raw' {
+	const content: string
+	export default content
+}
+
+declare module '@nextcloud/vue/dist/Components/*.js' {
+	import type { Component } from 'vue'
+	const component: Component
+	export default Component
+}
---
+++ templates/settings-admin.php
@@ -0,0 +1,6 @@
+<?php
+// SPDX-FileCopyrightText: 2023 Nextcloud GmbH <https://nextcloud.com/>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+?>
+
+<div id="admin-overview"></div>
